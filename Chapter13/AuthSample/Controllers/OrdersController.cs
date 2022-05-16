0cbb253f9074970392341e9a0fcbcad69e1c3647
Arun Tamirisa <arun.kumar.t@outlook.com>
2022-05-17 02:24:54 +0530

    Authorization Samples

---
+++ Chapter13/AuthSample/Controllers/OrdersController.cs
@@ -0,0 +1,16 @@
+﻿using AuthSample.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthSample.Controllers
+{
+    [Authorize(Policy = "Over14")]
+    public class OrdersController : Controller
+    {
+        [AuthorizeAgeOver(22)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Chapter13/AuthSample/Controllers/PremiumContentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthSample.Controllers
+{
+    [Authorize(Policy = "PremiumContentPolicy")]
+    public class PremiumContentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
